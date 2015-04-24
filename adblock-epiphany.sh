7bdfbc7938838628b36e2ed6d5599c08573ff3fe
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2015-04-24 08:59:12 +0800

    Finalised

--- adblock-epiphany.sh
+++ adblock-epiphany.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 flist=filter.list
-blist=8567481f1721260913812bf14bdb073b
+blist=~/.config/epiphany/adblock/8567481f1721260913812bf14bdb073b
 
+# initialise the filter list
+rm ~/.config/epiphany/adblock/*
+echo > $blist
 
-for filter in $flist; do
+while read filter;do
     curl $filter >> $blist
-done
+done < $flist
