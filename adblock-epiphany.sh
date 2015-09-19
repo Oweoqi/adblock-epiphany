3fcefbda9a82dcb5e3baabab583c00e0346334f1
Jimmy Jive <kuro@manticore.coz>
2015-09-19 10:15:02 +0800

    Updated a tonne of stupid settings

--- adblock-epiphany.sh
+++ adblock-epiphany.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-flist=~/.config/epiphany/adblock-epiphany/filter.list
+flist=~/.config/epiphany/adblock/filters.list
 blist=~/.config/epiphany/adblock/8567481f1721260913812bf14bdb073b
 
 # initialise the filter list
-rm ~/.config/epiphany/adblock/*
+cp ./filters.list $flist
 echo > $blist
 
 while read filter;do
