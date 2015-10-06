c8d66edebfe94c7c9e25a79a0d9050b1990181fc
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2015-10-06 13:08:12 +0800

    Update adblock-epiphany.sh

--- adblock-epiphany.sh
+++ adblock-epiphany.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
 flist=~/.config/epiphany/adblock/filters.list
-blist=~/.config/epiphany/adblock/8567481f1721260913812bf14bdb073b
 tmplist=~/.config/epiphany/adblock/temp
 
 # initialise the filter list
 cp ./filters.list $flist
-echo > $blist
 
-while read filter;do
-    curl $filter >> $tmplist
-done < $flist
-
-cat $tmplist | sort -u > $blist
-rm $tmplist
+epiphany
